436bc6762fe0a68a080639f1b488043916f01d18
Samuel Morales <sdmorales@uninorte.edu.co>
2021-11-15 14:36:51 -0500

    nuevo codigo

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hola mundo, grupo de programación 24")
+let numero = 8
+
+console.log(esPrimo(numero));
+
+function esPrimo(num){
+    let cont = 0
+    for (let i = 0; i <= num ; i++) {
+        if (num == 0){
+            cont = cont + 1 
+            //cont ++
+        }
+    }
+    if (cont ==2){
+        return true
+    }
+    return false
+}
